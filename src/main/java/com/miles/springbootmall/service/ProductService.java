c70668d03f643b14239d4f82d72a254f11f76399
hawn211215 <hawnadm@gmail.com>
2023-04-08 01:34:26 +0800

    商品功能 - 實作查詢商品列表 (基礎實作)

--- src/main/java/com/miles/springbootmall/service/ProductService.java
+++ src/main/java/com/miles/springbootmall/service/ProductService.java
@@ -3,8 +3,11 @@ package com.miles.springbootmall.service;
 import com.miles.springbootmall.dto.ProductRequest;
 import com.miles.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
 
+    List<Product> getProducts();
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
--- src/main/java/com/miles/springbootmall/service/impl/ProductServiceImpl.java
+++ src/main/java/com/miles/springbootmall/service/impl/ProductServiceImpl.java
@@ -7,9 +7,15 @@ import com.miles.springbootmall.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class ProductServiceImpl implements ProductService {
 
+    @Override
+    public List<Product> getProducts() {
+        return productDao.getProducts();
+    }
 
     @Autowired
     private ProductDao productDao;
